fa6cc911d451695c1d6fd50bcb5afcbf4b5a289d
VinlaRose <vinla.files@gmail.com>
2024-01-23 15:37:00 +0530

    added project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,11 +12,11 @@ export const Projects = () => {
             </div>
             <div className="item-b">
             <BoxWithDots title={"Social Media App"}
-            description={"A social media app having features like posting, editing post deleting, follow/unfollow, edit profile,login, signin features made using ReactJS."} githubLink={"https://github.com/VinlaRose/social-media-react"} projectLink={"https://slaybook-vin.netlify.app/"}/>
+            description={"A social media app having features like jwt authentication, posting, editing post deleting, follow/unfollow, edit profile,login, signin features made using ReactJS."} githubLink={"https://github.com/VinlaRose/social-media-react"} projectLink={"https://slaybook-vin.netlify.app/"}/>
             </div>
             <div className="item-c">
             <BoxWithDots title={"Ecom App"}
-            description={"A website for a furniture store including features like  filterling, sorting, cart, wishlist , payment etc."} githubLink={"https://github.com/VinlaRose/ecom-backend"} projectLink={"https://furnish-vin.netlify.app/"}/>
+            description={"A website for a furniture store including features like jwt authentication, filterling, sorting, cart, wishlist , payment etc."} githubLink={"https://github.com/VinlaRose/ecom-backend"} projectLink={"https://furnish-vin.netlify.app/"}/>
             </div>
             <div className="item-d">
             
@@ -24,6 +24,10 @@ export const Projects = () => {
             
             <div class="buttonProject" onClick={() => navigate("/allprojects")}  >See All Projects... </div>
             </div>
+            </div>
+            <div className="item-e">
+            <BoxWithDots title={"Data Visualization App"}
+            description={"This React website integrates an API layer and Data Pipeline for Google Sheets data, displaying it through bar graphs and line charts. A Cookie Management System stores user preferences, automatically applied upon revisiting. Users can reset preferences, and a shareable charts feature generates URLs for collaboration. "} githubLink={"https://github.com/VinlaRose/data-visualization-assignment-"} projectLink={"https://data-visualization-assignment.vercel.app/"}/>
             </div>
            
         </div>
